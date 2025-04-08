9969450653622e8aedacb5d96c78c399fb21424b
Fabrice Niyomwungeri <niomwungeri.fabrice@gmail.com>
2025-04-08 12:38:58 -0600

    add initial set up

---
+++ src/test/java/ca/reppy/reppy_backend/ReppyBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.reppy.reppy_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReppyBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
